515b7cf4c2c8e40db37abba9154196e0b91b8cde
Arivser <118214481+Arivser@users.noreply.github.com>
2022-11-23 18:56:30 +0100

    Cambiado
    He cambiado hola mundo por hei y tambien main por imprimeSaludo

--- ProyectoGitRamasEjemplo/src/actividad.java
+++ ProyectoGitRamasEjemplo/src/actividad.java
@@ -1,6 +1,6 @@
 
 public class actividad {
-	public static void main(String[] args) {
-		System.out.println("Hola mundo");
+	public static void imprimeSaludo() {
+		System.out.println("Hei");
 	}
 }
