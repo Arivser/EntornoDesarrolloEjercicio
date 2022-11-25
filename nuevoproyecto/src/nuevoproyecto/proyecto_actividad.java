610d565e79b1895d70a853bbbe1540c1e1498883
jriva <jriva@DESKTOP-8B4DKJQ>
2022-11-25 15:44:50 +0100

    Commit
    Se_le_ha_añadido_nueva_frase

--- nuevoproyecto/src/nuevoproyecto/proyecto_actividad.java
+++ nuevoproyecto/src/nuevoproyecto/proyecto_actividad.java
@@ -3,5 +3,6 @@ package nuevoproyecto;
 public class proyecto_actividad {
 	public static void main(String[] args) {
 		System.out.println("Buenos días");
+		System.out.println("Pronto habrá vacaciones");
 	}
 }
